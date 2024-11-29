ab172c8adaf45f4a58eca38419257d4bf2da26f1
Kobi <kobi@virtualbox.co.il>
2024-11-30 01:55:09 +0200

    Register FileDbContext and add necessary imports
    Added the registration of FileDbContext as a scoped service and included necessary imports for dependency injection and Entity Framework Core.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using FileRenamerProject;
+using Microsoft.Extensions.DependencyInjection;
+using FileRenamerProject.Data;
+using System.Net.Http;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Register the FileDbContext as a scoped service
+builder.Services.AddDbContext<FileDbContext>(options =>
+    options.UseSqlite("Data Source=files.db"));
 
 await builder.Build().RunAsync();
