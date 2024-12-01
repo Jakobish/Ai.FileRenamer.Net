499a88588cb78901eea4c7e7af3d88d77df1af72
kobi.shachar <kobi.shachar@gmail.com>
2024-12-01 00:57:19 +0000

    Add spacing for readability in NixOS configuration file

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,6 +3,8 @@
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
+  
+  
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
